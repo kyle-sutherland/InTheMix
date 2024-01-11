a1bba1a5321a84a7a2f7e412b47d9b2e4f2f59b2
velveteen-rever <velveteen.rever@gmail.com>
2024-01-11 15:12:53 -0700

    changed fonts

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,8 +1,6 @@
 import { LandingButton } from "@components/buttons";
 import Image from "next/image";
 
-
-
 export default function Landing() {
   return (
     <>
@@ -15,15 +13,11 @@ export default function Landing() {
           priority={true}
         />
         <div className="landingContent text-stone-50 mx-auto text-center py-48">
-          <div className="text-6xl p-3">
-            <p>
-              Pride in the Pour
-            </p>
+          <div className="landingTitle text-6xl p-3">
+            <p>Pride in the Pour</p>
           </div>
-          <div className="text-lg p-3 mb-4 pt-0">
-            <p>
-              Years of Experience, Quality Concoctions, Crafted Cocktails
-            </p>
+          <div className="landingSubtitle text-lg p-3 mb-4 pt-0">
+            <p>Years of Experience, Quality Concoctions, Crafted Cocktails</p>
           </div>
           <LandingButton text="CONTACT US" link="/#contact" />
         </div>
--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -15,8 +15,7 @@ function Sidebar() {
       <div className="bg-white"></div>
       <div className="bg-white">
         <div className="flex-col flex">
-          <div className="w-full border-b-2 border-gray-200">
-          </div>
+          <div className="w-full border-b-2 border-gray-200"></div>
           <div className="flex bg-gray-100  overflow-x-hidden">
             <div className="bg-white lg:flex md:w-64 md:flex-col hidden">
               <div className="flex-col pt-5 flex overflow-y-auto">
@@ -138,50 +137,89 @@ function Sidebar() {
                     {/*</div>*/}
                     <div>
                       {/*<p className="px-4 font-semibold text-xs tracking-widest text-gray-400 uppercase">Contact</p>*/}
-                      <div className="mt-4 bg-top bg-cover space-y-1">
-                        <a href="/#about" className="font-medium text-sm items-center rounded-lg text-gray-900 px-4 py-2.5 flex
-                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer">
+                      <div className="navigation mt-4 bg-top bg-cover space-y-1">
+                        <a
+                          href="/#about"
+                          className="font-medium items-center rounded-lg text-gray-900 px-4 py-2.5 flex
+                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer"
+                        >
                           <span className="justify-center items-center flex">
                             <span className="justify-center items-center flex">
                               <span className="justify-center items-center flex">
                                 <span className="items-center justify-center flex">
-                                  <svg className="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" /><path strokeLinecap="round"
-                                      strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0
+                                  <svg
+                                    className="flex-shrink-0 w-5 h-5 mr-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                  />
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0
                                 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0
-                                012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
+                                012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                                  />
                                 </span>
                               </span>
                             </span>
                           </span>
                           <span>About Us</span>
                         </a>
-                        <a href="/#cocktails" className="font-medium text-sm items-center rounded-lg text-gray-900 px-4 py-2.5 flex
-                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer">
+                        <a
+                          href="/#cocktails"
+                          className="font-medium items-center rounded-lg text-gray-900 px-4 py-2.5 flex
+                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer"
+                        >
                           <span className="justify-center items-center flex">
                             <span className="justify-center items-center flex">
                               <span className="justify-center items-center flex">
                                 <span className="items-center justify-center flex">
-                                  <svg className="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" /><path strokeLinecap="round"
-                                      strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0
-                                0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
+                                  <svg
+                                    className="flex-shrink-0 w-5 h-5 mr-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                  />
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0
+                                0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                                  />
                                 </span>
                               </span>
                             </span>
                           </span>
                           <span>Signature Cocktails</span>
                         </a>
-                        <a href="/#contact" className="font-medium text-sm items-center rounded-lg text-gray-900 px-4 py-2.5 flex
-                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer">
+                        <a
+                          href="/#contact"
+                          className="font-medium items-center rounded-lg text-gray-900 px-4 py-2.5 flex
+                          transition-all duration-200 hover:bg-gray-200 group cursor-pointer"
+                        >
                           <span className="justify-center items-center flex">
                             <span className="justify-center items-center flex">
                               <span className="justify-center items-center flex">
                                 <span className="items-center justify-center flex">
-                                  <svg className="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" /><path strokeLinecap="round"
-                                      strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0
-                                0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
+                                  <svg
+                                    className="flex-shrink-0 w-5 h-5 mr-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                  />
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0
+                                0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                                  />
                                 </span>
                               </span>
                             </span>
@@ -246,8 +284,7 @@ function Sidebar() {
           </div>
         </div>
       </div>
-    </div >
-
+    </div>
   );
 }
 
