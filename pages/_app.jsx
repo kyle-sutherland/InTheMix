d1ee0c3561e356deb08aa9321c660fb278cedfbd
velveteen-rever <velveteen.rever@gmail.com>
2024-01-26 19:51:49 -0700

    feat: added new next speed speed-insights package.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import { FormspreeProvider } from "@formspree/react";
 import "@styles/globals.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function Application({ Component, pageProps }) {
   return (
     <FormspreeProvider project={process.env.NEXT_PUBLIC_FORMSPREE_PROJECT}>
+    <SpeedInsights />
       <Component {...pageProps} />
     </FormspreeProvider>
   );
