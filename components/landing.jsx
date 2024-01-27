334db757d01ff7feaed308e32995aabc5ad70997
velveteen-rever <velveteen.rever@gmail.com>
2024-01-26 20:49:46 -0700

    fix: actually fixed image loading.

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,23 +1,10 @@
 import { LandingButton } from '@components/buttons'
-import { Suspense } from 'react'
 import Image from 'next/image'
-import mainAnimationPh from 'public/main-animation-ph.jpg'
 
 export default function Landing () {
-  const placeholder = () => {
-      <Image
-        src={ mainAnimationPh }
-        alt=''
-        className='landingImage'
-        fill
-        placeholder='blur'
-        eager
-      />
-  }
   return (
     <>
       <div className='landing bg-slate-300 opacity-75'>
-        <Suspense fallback={placeholder}>
           <Image
             src='/main-animation-avif.avif'
             className='landingImage'
@@ -25,7 +12,6 @@ export default function Landing () {
             alt=''
             eager
           />
-        </Suspense>
         <div className='landingContent text-stone-50 mx-auto text-center py-48'>
           <div className='landingTitle text-6xl p-3'>
             <p>Pride in the Pour</p>
