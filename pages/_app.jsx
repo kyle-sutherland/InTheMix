45e9fd5590d11e135d96c744b6ce2f22c96d9d6f
velveteen-rever <velveteen.rever@gmail.com>
2024-02-05 18:18:20 -0700

    fix: moved it again

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,11 @@
 import { FormspreeProvider } from '@formspree/react'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import '@styles/globals.scss'
 
 function Application ({ Component, pageProps }) {
   return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
-    >
-      <FormspreeProvider project={process.env.NEXT_PUBLIC_FORMSPREE_PROJECT}>
-        <Component {...pageProps} />
-      </FormspreeProvider>
-    </GoogleReCaptchaProvider>
+    <FormspreeProvider project={process.env.NEXT_PUBLIC_FORMSPREE_PROJECT}>
+      <Component {...pageProps} />
+    </FormspreeProvider>
   )
 }
 
