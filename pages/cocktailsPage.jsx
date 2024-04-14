a70574ae843020e53fc6143d2e309d9ba2ea606c
velveteen-rever <velveteen.rever@gmail.com>
2024-04-13 23:44:28 -0600

    fix: more ui tweaks

--- pages/cocktailsPage.jsx
+++ pages/cocktailsPage.jsx
@@ -1,11 +1,18 @@
 import Layout from 'pages/layout'
-import Fade from 'react-reveal/Fade'
+import { Fade } from 'react-awesome-reveal'
+import Image from 'next/image'
 
 export default function cocktailsPage () {
   return (
     <Layout>
       <div className='body-content '>
-        <div className='flex flex-col mx-auto container xl:max-w-7xl' />
+        <div className='flex flex-col mx-auto container xl:max-w-7xl'>
+          <div className='container cocktails-banner'>
+            <Fade>
+              <Image />
+            </Fade>
+          </div>
+        </div>
       </div>
     </Layout>
   )
