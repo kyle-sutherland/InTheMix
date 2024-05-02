678fd556b70415c3d8aa681d6dca05c4ff206cb6
velveteen-rever <velveteen.rever@gmail.com>
2024-05-02 14:59:34 -0700

    fix: fade animations, text formatting

--- pages/cocktails-page.jsx
+++ pages/cocktails-page.jsx
@@ -1,31 +1,31 @@
-import Layout from 'pages/layout'
-import { Fade } from 'react-awesome-reveal'
-import Image from 'next/image'
-import Link from 'next/link'
-import { allura, sofia, architects_daughter } from '../lib/fonts.js'
+import Layout from "/pages/layout";
+import { Fade } from "react-awesome-reveal";
+import Image from "next/image";
+import Link from "next/link";
+import { allura, sofia, architects_daughter } from "../lib/fonts.js";
 
-export default function CocktailsPage () {
-  const cocktails = require('data/signature-cocktails.json')
+export default function CocktailsPage() {
+  const cocktails = require("/data/signature-cocktails.json");
   return (
     <Layout>
-      <div className='body-content'>
-        <div className='flex flex-col mx-auto container'>
-          <div className='container cocktails-banner'>
+      <div className="body-content">
+        <div className="flex flex-col mx-auto container">
+          <div className="container cocktails-banner">
             <Image
-              src='/In-The-Mix(teal).svg'
+              src="/In-The-Mix(teal).svg"
               height={1000}
               width={1400}
-              alt='In The Mix teal logo'
+              alt="In The Mix teal logo"
             />
           </div>
           <Fade cascade damping={0.1} duration={1200} triggerOnce>
-            <div className='cocktails-h1 px-6'>
+            <div className="cocktails-h1 px-6">
               <t className={allura.className}>Signature Cocktails</t>
             </div>
-            <div className='cocktails-list'>
+            <div className="cocktails-list">
               {cocktails.map((liquor, j) => (
                 <div obj={liquor} key={j}>
-                  <div className='cocktails-h2'>
+                  <div className="cocktails-h2">
                     <t className={allura.className}>
                       {liquor.liquor} Cocktails
                     </t>
@@ -41,30 +41,30 @@ export default function CocktailsPage () {
                       <div
                         obj={cocktail}
                         key={i}
-                        className='grid grid-flow-row sm:grid-cols-3'
+                        className="grid grid-flow-row sm:grid-cols-3"
                       >
-                        <div className='sm:col-span-2'>
+                        <div className="sm:col-span-2">
                           <br />
-                          <div className='cocktails-name'>
+                          <div className="cocktails-name">
                             <p className={sofia.className}>{cocktail.name}</p>
                           </div>
-                          <div className='cocktails-description'>
+                          <div className="cocktails-description">
                             <p className={sofia.className}>
                               {cocktail.description}
                             </p>
                           </div>
-                          <div className='cocktails-ingredients'>
+                          <div className="cocktails-ingredients">
                             <p className={architects_daughter.className}>
-                              {cocktail.ingredients.join(', ')}
+                              {cocktail.ingredients.join(", ")}
                             </p>
                           </div>
                         </div>
-                        <div className='w-2/3 cocktails-icon'>
+                        <div className="w-2/3 cocktails-icon">
                           <Image
                             src={cocktail.image}
                             height={500}
                             width={500}
-                            alt='An icon representing the suggested glass'
+                            alt="An icon representing the suggested glass"
                           />
                         </div>
                       </div>
@@ -74,13 +74,13 @@ export default function CocktailsPage () {
               ))}
             </div>
           </Fade>
-          <div className='cocktails-h2 self-end bottom-0 pt-8'>
+          <div className="cocktails-h2 self-center bottom-0 pt-8">
             <t className={allura.className}>
-              <Link href='/#cocktails'>{'<<'} Back</Link>
+              <Link href="/#cocktails">{"<<"} Back</Link>
             </t>
           </div>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,77 +1,77 @@
-import Image from 'next/image'
-import ContactForm from '/components/contact-form'
-import Carousel from '/components/carousel'
-import Link from 'next/link'
-import Landing from '/components/landing'
-import Layout from 'pages/layout'
-import atumnSourImg from 'public/autumn-sour.jpg'
-import drink1Img from 'public/drink1.jpg'
-import drink2Img from 'public/drink2.jpg'
-import drink3Img from 'public/drink3.jpg'
-import drink4Img from 'public/drink4.jpg'
-import drink5Img from 'public/drink5.jpg'
-import groupDrinksImg from 'public/group-drinks.jpg'
-import firsideImg from 'public/fireside.jpg'
-import cocktailMenuImg from 'public/cocktail-menu.jpg'
-import { Fade } from 'react-awesome-reveal'
-import { allura } from '../lib/fonts.js'
+import Image from "next/image";
+import ContactForm from "/components/ContactForm";
+import Carousel from "/components/carousel";
+import Link from "next/link";
+import Landing from "/components/landing";
+import Layout from "/pages/layout";
+import atumnSourImg from "/public/autumn-sour.jpg";
+import drink1Img from "/public/drink1.jpg";
+import drink2Img from "/public/drink2.jpg";
+import drink3Img from "/public/drink3.jpg";
+import drink4Img from "/public/drink4.jpg";
+import drink5Img from "/public/drink5.jpg";
+import groupDrinksImg from "/public/group-drinks.jpg";
+import firsideImg from "/public/fireside.jpg";
+import cocktailMenuImg from "/public/cocktail-menu.jpg";
+import { Fade } from "react-awesome-reveal";
+import { allura } from "../lib/fonts.js";
 
-export default function Home () {
+export default function Home() {
   const cocktailCarouselImages = [
     {
       src: atumnSourImg,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: 'Autumn Sour',
-      desc: 'A thoughtfully crafted sour made with apple cider to kick off the harvest season. Ask about our curated cocktail menus.',
-      id: 1
+      title: "Autumn Sour",
+      desc: "A thoughtfully crafted sour made with apple cider to kick off the harvest season. Ask about our curated cocktail menus.",
+      id: 1,
     },
     {
       src: drink2Img,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: 'Red Rita',
-      desc: 'This well-balanced mix of sweet and cozy spices, with a hint of tartness from the cranberry juice. This cocktail is a flavorful and enjoyable option for any occasion.',
-      id: 2
+      title: "Red Rita",
+      desc: "This well-balanced mix of sweet and cozy spices, with a hint of tartness from the cranberry juice. This cocktail is a flavorful and enjoyable option for any occasion.",
+      id: 2,
     },
     {
       src: drink3Img,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: 'Slamma Jamma',
-      desc: 'It’s a perfect balance of tropical sweetness and spiced complexity, offering a refreshing and adventurous flavor profile. Enjoy a mini  vacation in a glass with every sip.',
-      id: 3
+      title: "Slamma Jamma",
+      desc: "It’s a perfect balance of tropical sweetness and spiced complexity, offering a refreshing and adventurous flavor profile. Enjoy a mini  vacation in a glass with every sip.",
+      id: 3,
     },
     {
       src: drink4Img,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: 'Fall Embrace',
-      desc: 'The fall embrace is perfect for those cool evenings, with warm  flavours of chai, cinnamon, and nutmeg.',
-      id: 4
+      title: "Fall Embrace",
+      desc: "The fall embrace is perfect for those cool evenings, with warm  flavours of chai, cinnamon, and nutmeg.",
+      id: 4,
     },
     {
       src: drink5Img,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: 'Summer Heat',
-      desc: 'This is a light and citrusy cocktail with a spicy kick that perfectly captures the essence of summer.',
-      id: 5
+      title: "Summer Heat",
+      desc: "This is a light and citrusy cocktail with a spicy kick that perfectly captures the essence of summer.",
+      id: 5,
     },
     {
       src: drink1Img,
-      alt: '',
+      alt: "",
       height: 500,
       width: 500,
-      title: '',
-      desc: '',
-      id: 6
-    }
+      title: "",
+      desc: "",
+      id: 6,
+    },
     // {
     //   src: "",
     //   alt: "",
@@ -81,34 +81,34 @@ export default function Home () {
     //   desc: "",
     //   id: 7,
     // },
-  ]
+  ];
   return (
     <>
       <Layout>
-        <Fade triggerOnce='true'>
+        <Fade triggerOnce="true">
           <Landing />
         </Fade>
-        <div className='body-content '>
-          <div className='flex flex-col mx-auto container xl:max-w-7xl'>
+        <div className="body-content ">
+          <div className="flex flex-col mx-auto container xl:max-w-7xl">
             <Fade cascade duration={1200} damping={0.1} fraction={0.1}>
-              <div id='about'>
+              <div id="about">
                 <h2>ABOUT US</h2>
-                <div className='container'>
-                  <div className='container grid md:grid-cols-2'>
-                    <div className='about-image order-first basis-1/2 py-3 ps-3'>
+                <div className="container">
+                  <div className="container grid md:grid-cols-2">
+                    <div className="about-image order-first basis-1/2 py-3 ps-3">
                       <Image
                         src={cocktailMenuImg}
-                        sizes='100vw'
+                        sizes="100vw"
                         width={500}
                         height={300}
                         style={{
-                          width: '100%',
-                          height: 'auto'
+                          width: "100%",
+                          height: "auto",
                         }}
-                        alt=''
+                        alt=""
                       />
                     </div>
-                    <div className='container oder-last text-center basis-1/2 p-3'>
+                    <div className="container oder-last text-center basis-1/2 p-3">
                       <h3>Cocktails with Experience</h3>
                       <p>
                         In The Mix was founded by Robyn Sutherland. She began
@@ -123,8 +123,8 @@ export default function Home () {
                       </p>
                     </div>
                   </div>
-                  <div className='container grid md:grid-cols-2'>
-                    <div className='container md:order-first text-center order-last basis-1/2 p-3'>
+                  <div className="container grid md:grid-cols-2">
+                    <div className="container md:order-first text-center order-last basis-1/2 p-3">
                       <h3>Bar Services for Your Event</h3>
                       <p>In The Mix offers a range of services including:</p>
                       <li>Hosted bar service</li>
@@ -136,20 +136,20 @@ export default function Home () {
                         so we may tailor our services accordingly.
                       </p>
                     </div>
-                    <div className='about-image md:order-last order-first basis-1/2 p-3'>
+                    <div className="about-image md:order-last order-first basis-1/2 p-3">
                       <Image
                         src={groupDrinksImg}
                         height={300}
                         width={500}
-                        alt=''
+                        alt=""
                       />
                     </div>
                   </div>
-                  <div className='container grid md:grid-cols-2'>
-                    <div className='about-image order-first basis-1/2 p-3'>
-                      <Image src={firsideImg} height={300} width={500} alt='' />
+                  <div className="container grid md:grid-cols-2">
+                    <div className="about-image order-first basis-1/2 p-3">
+                      <Image src={firsideImg} height={300} width={500} alt="" />
                     </div>
-                    <div className='container order-last basis-1/2 text-center p-3'>
+                    <div className="container order-last basis-1/2 text-center p-3">
                       <h3>Event Extras and Add-ons</h3>
                       <p>
                         We provide all of the non-alcoholic beverages, mixes,
@@ -161,37 +161,37 @@ export default function Home () {
                   </div>
                 </div>
               </div>
-              <div className='flex flex-col'>
-                <div id='cocktails' className='container cocktails'>
+              <div id="cocktails" className="flex flex-col">
+                <div className="container cocktails">
                   <h4 className={allura.className}>
-                    <Link href='/cocktails-page'>Signature Cocktails</Link>
+                    <Link href="/cocktails-page">Signature Cocktails</Link>
                   </h4>
                   {/* Cocktails-list */}
                   <Carousel images={cocktailCarouselImages} />
                 </div>
                 <h5 className={allura.className}>
-                  <Link href='/cocktails-page'>Full List {'>>'}</Link>
+                  <Link href="/cocktails-page">Full List {">>"}</Link>
                 </h5>
               </div>
-              <div id='contact' className='flex flex-col container'>
+              <div id="contact" className="flex flex-col container">
                 <h2>CONTACT US</h2>
                 {/* Contact */}
-                <div className='container grid md:grid-cols-2'>
-                  <div className='md:basis-1/2 p-3'>
-                    <p className='description'>
+                <div className="container grid md:grid-cols-2">
+                  <div className="md:basis-1/2 p-3">
+                    <p className="description">
                       Send me a message about your upcoming event. Do you need
                       help with permits? Can we design a customized menu for
                       you? Let me know, and I will get back to you soon.
                     </p>
                     <h1>In The Mix</h1>
                     <p>
-                      <Link href='#contact'>587-893-2204</Link>
+                      <Link href="#contact">587-893-2204</Link>
                     </p>
                     <p>
-                      <Link href='#contact'>inthemixcalgary@gmail.com</Link>
+                      <Link href="#contact">inthemixcalgary@gmail.com</Link>
                     </p>
                   </div>
-                  <div className='md:order-first md:basis-1/2 p-3'>
+                  <div className="md:order-first md:basis-1/2 p-3">
                     <ContactForm />
                   </div>
                 </div>
@@ -201,5 +201,5 @@ export default function Home () {
         </div>
       </Layout>
     </>
-  )
+  );
 }
